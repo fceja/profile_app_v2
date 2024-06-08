e8078530beabdd4cca772ce79399047c6185f93c
fceja <88167774+fceja@users.noreply.github.com>
2024-06-07 19:30:17 -0700

    wip, css styles for smaller devices

--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -3,10 +3,22 @@ import "@scss/pages/HomePage.scss";
 export default function HomePage() {
     return (
         <div className="home-page-div">
-            <div className="main-div">
+            <div className="hero-div">
+                <div className="left-right-div">
+                    <div className="left-div">
+                        <div className="heading-div">Get the <br />job done.<br /> Right.</div>
+                    </div>
+                    <div className="right-div">
+                        <div className="trapezoid-img-div">
+                            <img
+                                className="img-main"
+                                src="src/assets/temp_4x3.png"
+                                alt="test"
+                            />
+                        </div>
+                    </div>
+                </div>
                 <div className="trapezoid-div"></div>
-                <div className="heading-div">Get the job done. Right.</div>
-                <div className="trapezoid-img-div"></div>
             </div>
         </div>
     );
