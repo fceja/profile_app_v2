c44da0f5b1f88919e926ae8a54b30047884d8d46
fceja <88167774+fceja@users.noreply.github.com>
2024-06-10 13:38:23 -0700

    add text styles for all devices

--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -5,7 +5,21 @@ export default function HomePage() {
     return (
         <div className="home-page-div">
             <Hero />
-            <div className="main-div">Main Div</div>
+            <div className="content-div">
+                <section className="comp-info">
+                    <span className="statement">
+                        Trusted experts in residential and commercial electrical services. <br /> Ensuring quality and safety for your home or business.
+                    </span>
+                    <span className="serv">
+                        <div className="serv-title"><i>Services provided</i>: </div>
+                        <div className="serv-list">lighting, panel, outlets, wiring / re-wiring, maintenance, repairs, inspections, commercial kitchen, HVAC, electrical room, backup power. Don't see the service you're looking for? Please ask!</div>
+                    </span>
+                </section>
+                <section className="clients">
+                    <div>Some of our clients:</div>
+                    <div>Company A, Company B, Company C</div>
+                </section>
+            </div>
         </div>
     );
 }
