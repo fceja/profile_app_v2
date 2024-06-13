e1f5499097c1acd2081ecf7ffc272282f4ab4bab
fceja <88167774+fceja@users.noreply.github.com>
2024-06-12 18:52:38 -0700

    font adjustments

--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -9,11 +9,11 @@ export default function HomePage() {
             <div className="content-div">
                 <section className="comp-info">
                     <span className="statement">
-                        Trusted experts in residential and commercial electrical services. <br /> Ensuring quality and safety for your home or business.
+                        Trusted experts in <span className="highlight">residential</span> and <span className="highlight">commercial</span> electrical services. <br /> <br /> Ensuring <span className="highlight">quality</span> and <span className="highlight">safety</span> for your home or business.
                     </span>
                     <span className="serv">
-                        <div className="serv-title"><i>Services provided</i>: </div>
-                        <div className="serv-list">lighting, panel, outlets, wiring / re-wiring, maintenance, repairs, inspections, commercial kitchen, HVAC, electrical room, backup power. Don&#39;t see the service you&#39;re looking for? Please ask!</div>
+                        <div className="serv-title"><i>Services</i>: </div>
+                        <div className="serv-list">lighting, panel, outlets, wiring / re-wiring, maintenance, repairs, inspections, new construction, commercial kitchen, HVAC, electrical room, backup power. Don&#39;t see the service you&#39;re looking for? Please ask!</div>
                     </span>
                 </section>
                 <section>
