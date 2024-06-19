ad2b00111ecf3de1a737822e2091347f0440b6f6
fceja <88167774+fceja@users.noreply.github.com>
2024-06-19 15:22:29 -0700

    separate into content

---
+++ src/ts/content/GoogleMapContent.ts
@@ -0,0 +1,7 @@
+export const GoogleMapData = Object.freeze({
+    latitude: 36.778,
+    longitude: -119.417,
+    zoom: 5,
+    width: "100%",
+    height: "300px",
+});
---
+++ src/ts/content/HomeContent.ts
@@ -0,0 +1,28 @@
+
+export const cardsContent =
+    [
+        {
+            image: "src/assets/clients/PaniniKabobGrill.png",
+            title: "Title 1",
+            description: "Panini Kabob Grill",
+            urlLink: "https://paninikabobgrill.com/",
+        },
+        {
+            image: "src/assets/PopsAndSonsElectricLogo.png",
+            title: "Pops & Sons",
+            description: "Test",
+            urlLink: "todo",
+        },
+        {
+            image: "src/assets/PopsAndSonsElectricLogo.png",
+            title: "Pops & Sons",
+            description: "Test",
+            urlLink: "todo",
+        },
+        {
+            image: "src/assets/PopsAndSonsElectricLogo.png",
+            title: "Pops & Sons",
+            description: "Test",
+            urlLink: "todo",
+        },
+    ]
