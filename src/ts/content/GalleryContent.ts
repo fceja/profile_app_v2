d3f5992885eef7721a4508dd9a74bbf0333e5ff3
fceja <88167774+fceja@users.noreply.github.com>
2024-07-03 09:09:40 -0700

    update slidng issue where non-centered images where visible

--- src/ts/content/GalleryContent.ts
+++ src/ts/content/GalleryContent.ts
@@ -9,18 +9,18 @@ export const imageData = [
     },
     {
         original:
-            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/original/commercial-1.1_4x3.png",
+            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/original/commercial-6.2_4x3.png",
         thumbnail:
-            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/thumbnail/commercial-1.1_4x3_thumbnail.png",
-        title: "commercial-1.1_4x3",
-        alt: "commercial-1.1_4x3",
+            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/thumbnail/commercial-6.2_4x3_thumbnail.png",
+        title: "commercial-6.2_4x3",
+        alt: "commercial-6.2_4x3",
     },
     {
         original:
-            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/original/commercial-1.1_4x3.png",
+            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/residential/original/residential-2.1_1x1.png",
         thumbnail:
-            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/commercial/thumbnail/commercial-1.1_4x3_thumbnail.png",
-        title: "commercial-1.1_4x3",
-        alt: "commercial-1.1_4x3",
+            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/residential/thumbnail/residential-2.1_1x1_thumbnail.png",
+        title: "residential-2.1_1x1",
+        alt: "residential-2.1_1x1",
     },
 ]
