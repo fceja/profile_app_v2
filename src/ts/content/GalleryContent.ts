5aae4a5725efece4c965a1a26760c19bf4286f09
fceja <88167774+fceja@users.noreply.github.com>
2024-08-18 00:28:33 -0700

    update to gallery page styles

--- src/ts/content/GalleryContent.ts
+++ src/ts/content/GalleryContent.ts
@@ -17,10 +17,10 @@ export const imageData = [
     },
     {
         original:
-            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/residential/original/residential-2.1_1x1.png",
+            "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/residential/original/residential-2.1_4x3.png",
         thumbnail:
             "https://s3.us-west-1.amazonaws.com/dev.assets.pops-and-sons-electric/images/residential/thumbnail/residential-2.1_1x1_thumbnail.png",
-        title: "residential-2.1_1x1",
+        title: "residential-2.1_4x3",
         alt: "residential-2.1_1x1",
     },
 ]
