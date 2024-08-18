d0e2c31e1924dda570628e81e093ca6bf74ead2b
fceja <88167774+fceja@users.noreply.github.com>
2024-08-17 22:30:51 -0700

    updates to make sure images are loaded before rendering

--- src/ts/pages/GalleryPage.tsx
+++ src/ts/pages/GalleryPage.tsx
@@ -1,19 +1,72 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 
 import "@scss/pages/GalleryPage.scss";
 import { imageData } from "@content/GalleryContent"
 import Loading from "@common/components/Loading";
+import Error from "@common/components/Error";
 
 const ImageGallery = lazy(() => import("react-image-gallery"));
 
 const MyImageGallery = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+    const [imgLoaded, setImgLoaded] = useState(false)
+
+    /* checks to see if image element is loaded */
+    useEffect(() => {
+        const checkImgLoaded = (count: number) => {
+            const imgDiv = document.querySelector('.image-gallery-center')
+
+            if (imgDiv) {
+                const imgElement = imgDiv.querySelector('img'); // Select the image within the container
+
+                if (imgElement) {
+                    if (imgElement.complete) {
+                        setImgLoaded(true)
+                        return
+                    }
+                }
+            }
+
+            /* 3 sec, 12 tries at 250ms intervals */
+            if (count < 12) {
+                setTimeout(() => checkImgLoaded(count + 1), 250);
+            } else {
+                console.warn('error loading image')
+                setIsError(true)
+            }
+        }
+
+        checkImgLoaded(0)
+
+    }, [])
+
+    /* if image element loaded, make image gallery element visible */
+    useEffect(() => {
+        if (imgLoaded) {
+            const imgGalleryDiv = document.querySelector('.image-gallery') as HTMLElement
+
+            if (imgGalleryDiv) {
+                imgGalleryDiv.style.display = "block"
+                setIsLoading(false)
+            }
+
+        }
+
+    }, [imgLoaded])
+
     return (
         <main className="app-react-image-gallery mt-3">
-            <Suspense fallback={<Loading className="image-gallery" />}>
-                <ImageGallery items={imageData} />
-            </Suspense>
+            <>
+                {isLoading && <Loading className="image-gallery" />}
+                {isError && <Error />}
+                <Suspense fallback={<Loading className="image-gallery" />}>
+                    <ImageGallery items={imageData} />
+                </Suspense>
+            </>
         </main>
     );
 };
 
 export default MyImageGallery;
+
